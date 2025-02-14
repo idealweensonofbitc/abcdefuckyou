6a1c1ec247f8baf98ca0081419fbfc1e2ee8026f
idealweensonofbitc <idealweensonofbitc@gmail.com>
2025-02-14 22:38:15 +0330

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-const check=false;if(check){setTimeout(() =>{const body = document.body;body.remove()},10000)}
+const check=true;if(check){setTimeout(() =>{const body = document.body;body.remove()},10000)}
